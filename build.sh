7ac5b066f2deaf83fe4248c808c7d1a7399bcf7a
cfstras <cfstras@users.noreply.github.com>
2022-11-18 13:35:06 +0100

    update for latest minecraft

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # build from source for maximum performance
 apt update
-apt install -y git python3-pip python3-pil python3-dev python3-numpy wget
+apt install -y git python3-pip python3-pil python3-dev python3-numpy curl
 
 overviewer=/overviewer
 
@@ -11,18 +11,22 @@ if [[ -d $overviewer ]]; then
     cd $overviewer
     git pull
 else
-    git clone git://github.com/overviewer/Minecraft-Overviewer.git $overviewer
+    git clone https://github.com/overviewer/Minecraft-Overviewer.git $overviewer
     cd $overviewer
 fi
 
+# note: only really necessary if we installed pillow with pip
+# curl -fsSLO https://github.com/python-pillow/Pillow/raw/9.3.0/src/libImaging/ImPlatform.h
+# curl -fsSLO https://github.com/python-pillow/Pillow/raw/9.3.0/src/libImaging/Imaging.h
+# curl -fsSLO https://github.com/python-pillow/Pillow/raw/9.3.0/src/libImaging/ImagingUtils.h
 
 export CFLAGS="-Ofast"
 python3 setup.py build
 
-VERSION=1.15
+VERSION=1.19.2
 TEXTURE_FILE=~/.minecraft/versions/${VERSION}/${VERSION}.jar
 
 if [[ ! -s $TEXTURE_FILE ]]; then
     mkdir -p ~/.minecraft/versions/${VERSION}/
-    wget https://overviewer.org/textures/${VERSION} -O $TEXTURE_FILE
+    curl -fSL https://overviewer.org/textures/${VERSION} -o $TEXTURE_FILE
 fi
